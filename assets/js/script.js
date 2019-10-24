a25c9ffc6c73edf23b848fa063219827cbf2e9d2
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-10-24 16:57:52 +0200

    changement js dernière minute

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ document.getElementById("image1").onmouseover = change();
 document.getElementById("image1").onmouseout = changeBack();
 
 function change() {
-  document.getElementById("image1").src = "../img/image1_2.jpg";
+  document.getElementById("image1").src = "./assets/img/image1_2.jpg";
 }
 function changeBack() {
-  document.getElementById("image1").src = "../img/image1.jpg";
+  document.getElementById("image1").src = "./assets/img/image1.jpg";
 }
