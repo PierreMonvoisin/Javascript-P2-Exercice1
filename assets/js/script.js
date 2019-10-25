50f77409b16d96801b84d1b5667c94c7228677bb
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-10-25 20:48:37 +0200

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let image1 = document.getElementById("image1");
 image1.onmouseenter = function() {
   image1.src = "./assets/img/image1_2.jpg";
 };
-image1.onmouseout = function() {
-  image1.src = "./assets/img/image1.jpg"
-};
+
+// image1.onmouseout = function() {
+  //   image1.src = "./assets/img/image1.jpg"
+  // };
