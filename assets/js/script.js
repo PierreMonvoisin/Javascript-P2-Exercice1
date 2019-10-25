43b64ecf30a99d0b5170d7ef38a65928656e197c
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-10-25 09:26:51 +0200

    Exercice 1 terminé (Faute de frappe dans déclaration des fonctions )

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-let image1 = document.getElementById("image1")//.object ?
+let image1 = document.getElementById("image1");
 
-image1.onmouseover = funtion (){ change() };
-image1.onmouseout = fuction() { changeBack() };
-
-function change() {
+image1.onmouseenter = function() {
   image1.src = "./assets/img/image1_2.jpg";
-}
-function changeBack() {
-  image1.src = "./assets/img/image1.jpg";
-}
+};
+image1.onmouseout = function() {
+  image1.src = "./assets/img/image1.jpg"
+};
